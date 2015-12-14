aff47a6ad7015e68ded73629ee57c978e67dcc07
Godwin Assumaning <godwinappiah30@yahoo.com>
2015-12-13 17:11:27 -0700

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,42 @@
+if(!file.exists('data.zip')){
+  url<-"http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+  
+  download.file(url,destfile = "data.zip")
+}
+
+if(!file.exists('data.zip')){
+  url<-"http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+  
+  download.file(url,destfile = "data.zip")
+}
+
+data<-read.table("household_power_consumption.txt",header = TRUE, sep= ";")
+
+names(data)
+lapply(data, class)
+
+#first few values of Date and Time
+data$Date[1:10]
+
+data$Time[1:10]
+
+#strptime to change the factor Date and Time values in to yyyy-mm-dd hh:mm:ss. First, let's create a variables by concatenating Date and Time.
+data$DateTime<-paste(data$Date, data$Time)
+
+#help document of strptime
+?strptime
+
+#Beginning of Project
+
+#change DateTime to yyyy-mm-dd hh:mm:ss
+data$DateTime<-strptime(data$DateTime, "%d/%m/%Y %H:%M:%S")
+
+#Data selection dates: 2007-02-01 and 2007-02-02.
+start<-which(data$DateTime==strptime("2007-02-01", "%Y-%m-%d"))
+
+end<-which(data$DateTime==strptime("2007-02-02 23:59:00", "%Y-%m-%d %H:%M:%S"))
+
+data2<-data[start:end,]
+
+plot(data2$DateTime, as.numeric(as.character(data2$Global_active_power)),
+     type='l',ylab="Global Active Power (Kilowatts)", xlab="")
